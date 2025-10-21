720a8d52a3ab16a482af9f27eb5d87ed32c36333
Gopal-Kundu <kundugopal2005@gmail.com>
2025-10-21 23:05:21 +0530

    Fixed a bug in login api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -91,7 +91,7 @@ export const login = async (req, res) => {
     return res.status(200).cookie("token",token, {maxAge: 7*24*60*60*1000, httpOnly: true, sameSite: 'strict'}).json({
       _id: user._id,
       username: user.username,
-      photo: user.profilephoto,
+      photo: user.profilePhoto,
       about: user.about,
     });
   } catch (error) {
