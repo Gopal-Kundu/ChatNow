e8e8b4aba344fa3f1a5c0eabefd7724cb460f332
Gopal-Kundu <kundugopal2005@gmail.com>
2026-01-11 12:01:25 +0530

    Deployed

--- frontend/address/address.js
+++ frontend/address/address.js
@@ -1,3 +1,1 @@
-export const baseurl = "http://localhost:2005";
-// export const baseurl = "https://82d14ce26741.ngrok-free.app";
-// export const baseurl = "http://10.167.185.240:2005";
+export const baseurl = "https://chat-now-webapp-backend.vercel.app";
