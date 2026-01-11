33078afe08afb7283ad4fe08fd0beaf686df36d0
Gopal Kundu <kundugopal2005@gmail.com>
2026-01-11 23:01:05 +0530

    Update InputBox.jsx

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -12,11 +12,18 @@ function InputBox({ theirId }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if(!socket) return;
-    socket.on("Msg from sender", (newMessage) => {
-      dispatch(setMsg(newMessage));
-    });
-  }, [dispatch]);
+  if (!socket) return;
+
+  const handleIncomingMessage = (newMessage) => {
+    dispatch(setMsg(newMessage));
+  };
+
+  socket.on("Msg from sender", handleIncomingMessage);
+
+  return () => {
+    socket.off("Msg from sender", handleIncomingMessage);
+  };
+}, [socket, dispatch]);
 
   function handleKeyDown(e) {
     if (e.key === "Enter" && !e.shiftKey) {
