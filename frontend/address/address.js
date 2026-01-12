590e8614ae5188af923229dd9e59b726848114c5
Gopal Kundu <kundugopal2005@gmail.com>
2026-01-12 11:04:06 +0530

    Update address.js

--- frontend/address/address.js
+++ frontend/address/address.js
@@ -1,2 +1,2 @@
-// export const baseurl = "https://chatnow-ns9r.onrender.com";
-export const baseurl = "http://localhost:2005";
+export const baseurl = "https://chatnow-ns9r.onrender.com";
+//export const baseurl = "http://localhost:2005";
