34cfcb784a05fd70d3a41cacfe6e1899d333a4e9
Gopal Kundu <kundugopal2005@gmail.com>
2026-01-16 20:55:37 +0530

    Chatsection bug fixed

--- frontend/src/components/ChatSection.jsx
+++ frontend/src/components/ChatSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import LoadingPage from "../pages/LoadingPage";
 import Message from "../components/Message";
 import { useSelector } from "react-redux";
@@ -10,11 +10,17 @@ function ChatSection({ theirId }) {
   const allMsgs = useSelector((state) => state.chat?.msgContainer || []);
   const { id } = useParams();
 
+  const bottomRef = useRef(null);
+
   const filteredMsgs = allMsgs.filter(
     (msg) =>
       (msg.receiverId === id && msg.senderId === user._id) ||
-      (msg.senderId === id && msg.receiverId === user._id) 
+      (msg.senderId === id && msg.receiverId === user._id)
   );
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [filteredMsgs.length]);
 
   return (
     <div className="webkit-scrollbar flex-1 overflow-y-auto">
@@ -22,13 +28,15 @@ function ChatSection({ theirId }) {
         <LoadingPage />
       ) : (
         <div>
-          {filteredMsgs?.map((msg, idx) => (
+          {filteredMsgs.map((msg, idx) => (
             <Message
               key={idx}
               user={theirId === msg?.senderId ? "false" : "true"}
               text={msg?.message}
             />
           ))}
+
+          <div ref={bottomRef} />
         </div>
       )}
     </div>
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 function Message({ user, text }) {
-  const scrollRef = useRef();
-  useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [text]);
-
   return (
     <div
-      ref={scrollRef}
       className={`flex max-w-screen my-1 ${
         user === "true" ? "mr-10 justify-end" : "ml-10 justify-start"
       }`}
